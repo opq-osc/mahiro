073cef84b2cfc3da54de711a57c6cd32a607a148
fz6m <59400654+fz6m@users.noreply.github.com>
2023-04-23 03:08:32 +0800

    chore: remove auto add data prefix

--- packages/mahiro/src/utils/file.ts
+++ packages/mahiro/src/utils/file.ts
@@ -2,7 +2,6 @@ import { consola } from 'consola'
 import { existsSync, readFileSync, writeFileSync } from 'fs'
 import { readFile } from 'fs/promises'
 import isBase64 from 'is-base64'
-import { type MimeType, fileTypeFromBuffer } from 'mahiro/compiled/file-type'
 
 const logger = consola.withTag('file-utils') as typeof consola
 
@@ -11,14 +10,6 @@ export interface IDetectFileType {
   filePath?: string
   base64?: string
 }
-
-const ALLOW_MIME_TYPES: MimeType[] = [
-  // image
-  'image/jpeg',
-  'image/png',
-  'image/gif',
-  'image/webp',
-]
 
 export const detectFileType = async (url: string): Promise<IDetectFileType> => {
   const isUrl = /^http/.test(url)
@@ -29,8 +20,9 @@ export const detectFileType = async (url: string): Promise<IDetectFileType> => {
   }
   const isDataFile = /^data:/.test(url)
   if (isDataFile) {
+    const urlWithoutData = url.replace(/^data:.*?;base64,/, '')
     return {
-      base64: url,
+      base64: urlWithoutData,
     }
   }
   // check is local file
@@ -43,33 +35,10 @@ export const detectFileType = async (url: string): Promise<IDetectFileType> => {
   } catch {}
   // check base64 url
   const isBase64Url = isBase64(url)
-  // we auto transform to base64 with data prefix
   if (isBase64Url) {
-    logger.info(
-      `Use base64 to send image, but without 'data:' prefix, we will try auto transform it.`,
-    )
-    try {
-      // https://gist.github.com/tuananh/63aa569531839c920e9cfe472eb17e35
-      const res = await fileTypeFromBuffer(Buffer.from(url, 'base64'))
-      const mime = res?.mime
-      if (mime?.length) {
-        const isLegalMime = ALLOW_MIME_TYPES.includes(mime)
-        if (!isLegalMime) {
-          logger.error(`Not allow mime type: ${mime}!`)
-          return {}
-        }
-        const base64WithDataPrefix = `data:${mime};base64,${url}`
-        logger.success(
-          `Auto transform base64 to data url success!, mime: ${mime}`,
-        )
-        return {
-          base64: base64WithDataPrefix,
-        }
-      } else {
-        logger.error(`Detect file mime type failed`)
-      }
-    } catch (_) {
-      logger.error(`Auto transform base64 to data url failed`)
+    logger.debug('is like base64 url')
+    return {
+      base64: url,
     }
   }
   return {}
