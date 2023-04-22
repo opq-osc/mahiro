7802bc9d65703e1f8bc983a8e82a75d549a2cc9d
fz6m <59400654+fz6m@users.noreply.github.com>
2023-04-22 20:48:20 +0800

    example: add meme demo

--- python/main.py
+++ python/main.py
@@ -9,11 +9,13 @@ mahiro = Mahiro()
 from plugins.chinchin_pk_mahiro.main import chinchin_pk
 from plugins.opqqq_plugins_mahiro.src.bot_good_morning import bot_good_morning
 from plugins.opqqq_plugins_mahiro.src.bot_sign_in import bot_sign_in
+from plugins.memes.memes import memes
 
 # load plugins
 mahiro.container.add_group(id="牛了个牛", callback=chinchin_pk)
 mahiro.container.add_group(id="早晚安", callback=bot_good_morning)
 mahiro.container.add_group(id="签到", callback=bot_sign_in)
+mahiro.container.add_group(id="meme", callback=memes)
 
 # load firend plugin
 from plugins.friend_simple_mahiro.main import friend_simple_mahiro
