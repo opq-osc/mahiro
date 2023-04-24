182288f671a3660c54abcaa00dbc87c88f591703
fz6m <59400654+fz6m@users.noreply.github.com>
2023-04-25 03:20:24 +0800

    docs: add api desc

---
+++ packages/mahiro/src/core/avatar.ts
@@ -0,0 +1,7 @@
+class Avatar {
+  constructor() {}
+
+  // todo: support avatar utils
+  // refer: https://wiki.connect.qq.com/%E7%94%A8%E6%88%B7%E5%9F%BA%E6%9C%AC%E4%BF%A1%E6%81%AF%E4%B8%AD%E7%9A%84%E7%94%A8%E6%88%B7%E5%A4%B4%E5%83%8Furl%E5%9F%9F%E5%90%8D%E5%8F%98%E6%9B%B4%E9%80%9A%E7%9F%A5
+  //        https://cloud.tencent.com/developer/article/1987577
+}
